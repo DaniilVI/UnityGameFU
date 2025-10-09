17495d58ae2bab5ec80beb632211ed17ddb89201
Aspeed123 <vladbliznyakov123@gmail.com>
2025-10-09 19:20:05 +0300

    Добавлен рывок, уменьшение и удар (#14)
    * Added dash, reduction and hit
    
    * Была переработана механика удара, рывок корректно работает, уменьшение/увеличение теперь не работает в воздухе
    
    * был удалён ненужный AttackEffect.prefab, сила рывка была изменена с 20 до 60, Vector2 был везде заменен на Vector3
    
    ---------
    
    Co-authored-by: DaniilVI <vahtanovdaniil@gmail.com>

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -1,40 +1,93 @@
 using UnityEngine;
+using System.Collections;
 
 public class CharacterMove : MonoBehaviour
 {
-
+    [Header("Движение")]
+    [SerializeField] private GameObject spriteObject;
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 13f;
+
+    [Header("Рывок (Dash)")]
+    [SerializeField] private float dashForce = 60f;
+    [SerializeField] private float dashDuration = 0.2f;
+    [SerializeField] private float dashCooldown = 1f;
+    private bool isDashing = false;
+    private bool canDash = true;
+
+    [Header("Удар")]
+    [SerializeField] private GameObject boxGloveObject;
+    [SerializeField] private float attackDuration = 0.5f;
+    [SerializeField] private float attackCooldown = 0.5f;
+    private bool canAttack = true;
     private bool isJump = false;
+    private bool isSmall = false;
+    private bool isFrozen = false;
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private SpriteRenderer boxGloveSprite;
     private Vector3 inputDirection;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (spriteObject == null)
+        {
+            Debug.LogError("Sprite object is not assigned!");
+            return;
+        }
+        if (boxGloveObject == null)
+        {
+            Debug.LogError("Box Glove object is not assigned!");
+            return;
+        }
+
         rb = GetComponent<Rigidbody2D>();
-        sprite = GetComponentInChildren<SpriteRenderer>();
+        sprite = spriteObject.GetComponent<SpriteRenderer>();
+        boxGloveSprite = boxGloveObject.GetComponentInChildren<SpriteRenderer>();
+        boxGloveObject.SetActive(false);
     }
 
     void FixedUpdate()
     {
-        Run();
+        if (!isFrozen && !isDashing)
+        {
+            Run();
+        }
         CheckJump();
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckRun();
-        if (!isJump && Input.GetButtonDown("Jump"))
-        {
-            Jump();
-        }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
+        }
+
+        if (isDashing) return;
+        if (isFrozen) return;
+
+        CheckRun();
+        if (Input.GetButtonDown("Jump") && !isJump)
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && !isJump)
+        {
+            ToggleSize();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(Attack());
         }
     }
 
@@ -53,6 +106,18 @@ public class CharacterMove : MonoBehaviour
         if (inputDirection.x != 0)
         {
             sprite.flipX = inputDirection.x < 0;
+            boxGloveSprite.flipX = inputDirection.x < 0;
+
+            Vector3 localPos = boxGloveObject.transform.localPosition;
+            if (boxGloveSprite.flipX)
+            {
+                localPos.x = -Mathf.Abs(localPos.x);
+            }
+            else
+            {
+                localPos.x = Mathf.Abs(localPos.x);
+            }
+            boxGloveObject.transform.localPosition = localPos;
         }
     }
 
@@ -65,5 +130,72 @@ public class CharacterMove : MonoBehaviour
     {
         Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
         isJump = collider.Length <= 1;
+    }
+
+    private IEnumerator Dash()
+    {
+        if (!canDash) yield break;
+
+        canDash = false;
+        isDashing = true;
+
+        float origGravity = rb.gravityScale;
+        float origDrag = rb.drag;
+
+        float direction = sprite.flipX ? -1f : 1f;;
+
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector3(direction * dashForce, 0f, 0f);
+
+        rb.drag = 8f;
+
+        yield return new WaitForSeconds(dashDuration);
+
+        rb.velocity = Vector3.zero;
+
+        rb.drag = origDrag;
+        rb.gravityScale = origGravity;
+
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+
+    private void ToggleSize()
+    {
+        if (isSmall)
+        {
+            transform.localScale *= 2f;
+            isSmall = false;
+        }
+        else
+        {
+            transform.localScale *= 0.5f;
+            isSmall = true;
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        if (!canAttack)
+            yield break;
+        canAttack = false;
+
+        isFrozen = true;
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = true;
+
+        boxGloveObject.SetActive(true);
+
+        yield return new WaitForSeconds(attackDuration);
+
+        boxGloveObject.SetActive(false);
+
+        rb.isKinematic = false;
+        isFrozen = false;
+
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
     }
 }
