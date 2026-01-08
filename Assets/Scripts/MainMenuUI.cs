65015135a9dd67f96d99c5337c0fe181dbf7e922
Aspeed123 <vladbliznyakov123@gmail.com>
2026-01-08 16:28:41 +0300

    были добавлены настройки звука

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
     [Header("Основные панели")]
     [SerializeField] private GameObject panelExitConfirm;
+    [SerializeField] private AudioMixer audioMixer;
 
     private void Start()
     {
         ShowMainMenu();
+        ApplyVolume();
     }
 
     public void OnPlayButton()
@@ -46,5 +49,21 @@ public class MainMenuUI : MonoBehaviour
     private void ShowExitConfirm()
     {
         panelExitConfirm.SetActive(true);
+    }
+
+    private void ApplyVolume()
+    {
+        float savedVolume = PlayerPrefs.GetFloat("SFX_VOLUME", 1f);
+
+        if (savedVolume <= 0.0001f)
+        {
+            audioMixer.SetFloat("SFX_Volume", -80f);
+        }
+        else
+        {
+            float dB = Mathf.Log10(savedVolume) * 20f;
+            audioMixer.SetFloat("SFX_Volume", dB);
+            Debug.Log($"SFX slider={savedVolume}, dB={dB}");
+        }
     }
 }
